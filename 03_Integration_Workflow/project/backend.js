b746b3af32ff8cfd39bc3510b62a75e09a095797
Sean Crowe <retrounlocked@gmail.com>
2023-06-09 13:19:07 -0500

    🚧 Update Ch 3 with skeleton of backend

--- 03_Integration_Workflow/project/backend.js
+++ 03_Integration_Workflow/project/backend.js
@@ -1,5 +1,5 @@
 export function authentication() {
-
+ return "hi"
 }
 
 
--- 03_Integration_Workflow/project/server.js
+++ 03_Integration_Workflow/project/server.js
@@ -1,48 +1,71 @@
-import http from 'http';
-import  fs from 'fs';
-import path from 'path';
-import {authentication} from './backend.js';
-import { fileURLToPath } from 'url';
+import http from "http";
+import  fs from "fs";
+import path from "path";
+import {authentication} from "./backend.js";
+import { fileURLToPath } from "url";
 
 const port = 3000; // Set the desired port number
 
-const dir = path.dirname(fileURLToPath(import.meta.url));
-
-const indexFilePath = path.join(dir, 'public/user.html'); // Path to your index.html file
+const publicPath = path.join(path.dirname(fileURLToPath(import.meta.url)), "public");
 
 const server = http.createServer((req, res) => {
 
-  console.log(req.url);
+  if (req.headers.cookie == null) {
 
-  // Check if the request is for the root path
-  if (req.url === '/') {
-    // Read the index.html file
-    fs.readFile(indexFilePath, 'utf8', (err, content) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-        console.error(err);
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-      }
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('File Not Found');
+    if (req.url == "/authentication") {
+      req.on("data", () => {
+        const apiKey = authentication();
+        res.writeHead(200, { "Content-Type": "text/plain", "Set-Cookie": `key=${apiKey}; Max-Age=180` });
+        res.end("Cookie Sent - You are Authorized");
+      });
+    }
+    else {
+      fs.readFile(`${publicPath}/user.html`, "utf8", (err, content) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content);
+        }
+      });
+    }
   }
+  else {
+    if (req.url == "/" || req.url == "/store" || req.url == "/authentication") {
 
-  if (req.url === '/authentication') {
+      fs.readFile(`${publicPath}/store.html`, "utf8", (err, content) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content);
+        }
+      });
+      
+    }
 
-    req.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
-    });
-
-    req.on("end", () => console.log("end"))
-    //authentication()
+    if (req.url === "/editor") {
+      fs.readFile(`${publicPath}/editor.html`, "utf8", (err, content) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content);
+        }
+      });
+      
+    }
   }
+ 
 });
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
